619f9160a263a0624d313dc7bcd948ee3b9a7f73
Alan Edlebeck <alan.edlebeck@gmail.com>
2022-07-16 15:57:12 -0500

    Currently running servers added

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const {ApolloServer} = require('apollo-server-express');
 
 const {typeDefs, resolvers} = require('./schemas');
 const {authMiddleware} = require('./utils/auth');
+const path = require('path');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -21,6 +22,14 @@ app.use(express.json());
 const startApolloServer = async(typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({app});
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 
   db.once('open', () => {
     app.listen(PORT, () => {
